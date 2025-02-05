acfe4be458855ed02284dff6353fcebf8d2fbfd0
Arnoldi, Loretta <loretta.arnoldi@nttdata.com>
2025-02-05 15:10:29 +0100

    add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-
+import { RouterModule } from '@angular/router';
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
   template: `
     <main> 
       <header class="brand-name"> 
@@ -12,7 +12,7 @@ import { HomeComponent } from './home/home.component';
         alt="logo" aria-hidden="true"> 
       </header> 
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>  
     </main> 
   `,
